9418226d4276b3279a415c5cd62ed3b27d54c79e
KiChang Shin <kc7851@gmail.com>
2019-11-24 02:38:08 +0900

    store, Provider 적용
    useSelector, useDispatch 등을 사용하기 위해 적용

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { Provider } from 'react-redux'
+import { StyleSheet, View } from "react-native";
+
+import HomeScreen from "./screens/HomeScreen";
+
+import store from "./store";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <HomeScreen />
+      </View>
+    </Provider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
 });

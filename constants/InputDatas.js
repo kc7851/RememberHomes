d4f9bb937e2e4a1b0315ac841d284f6d015c0909
KiChang Shin <kc7851@gmail.com>
2019-12-08 22:55:13 +0900

    list의 array의 항목을 따로 분리
    CustomRNPickerSelect의 props로 넣을 list를 따로 파일로 분리하여 관리

---
+++ constants/InputDatas.js
@@ -0,0 +1,93 @@
+export const floorArray = [
+    { key: "14층", label: "14층", value: "14" },
+    { key: "13층", label: "13층", value: "13" },
+    { key: "12층", label: "12층", value: "12" },
+    { key: "11층", label: "11층", value: "11" },
+    { key: "10층", label: "10층", value: "10" },
+    { key: "9층", label: "9층", value: "9" },
+    { key: "8층", label: "8층", value: "8" },
+    { key: "7층", label: "7층", value: "7" },
+    { key: "6층", label: "6층", value: "6" },
+    { key: "5층", label: "5층", value: "5" },
+    { key: "4층", label: "4층", value: "4" },
+    { key: "3층", label: "3층", value: "3" },
+    { key: "2층", label: "2층", value: "2" },
+    { key: "1층", label: "1층", value: "1" },
+    { key: "b1층", label: "b1층", value: "b1" },
+  ];
+
+  export const areaArray = [
+    { key: "5평", label: "5평", value: "5" },
+    { key: "7평", label: "7평", value: "7" },
+    { key: "10평", label: "10평", value: "10" },
+    { key: "12평", label: "12평", value: "12" },
+    { key: "15평", label: "15평", value: "15" },
+    { key: "18평", label: "18평", value: "18" },
+    { key: "21평", label: "21평", value: "21" },
+    { key: "25평", label: "25평", value: "25" },
+    { key: "28평", label: "28평", value: "28" },
+    { key: "33평", label: "33평", value: "33" },
+    { key: "36평", label: "36평", value: "36" },
+    { key: "40평", label: "40평", value: "40" },
+  ];
+  
+  export const directionArray = [
+    { key: "동향", label: "동향", value: "동향" },
+    { key: "서향", label: "서향", value: "서향" },
+    { key: "남향", label: "남향", value: "남향" },
+    { key: "북향", label: "북향", value: "북향" },
+    { key: "남동향", label: "남동향", value: "남동향" },
+    { key: "남서향", label: "남서향", value: "남서향" },
+    { key: "북동향", label: "북동향", value: "북동향" },
+    { key: "북서향", label: "북서향", value: "북서향" },
+  ];
+  
+  export const housetypeArray = [
+    { key: "빌라", label: "빌라", value: "빌라" },
+    { key: "오피스텔", label: "오피스텔", value: "오피스텔" },
+    { key: "아파트", label: "아파트", value: "아파트" },
+    { key: "다가구주택", label: "다가구주택", value: "다가구주택" },
+    { key: "다세대주택", label: "다세대주택", value: "다세대주택" },
+  ];
+
+  export const housestructureArray = [
+    { key: "원룸(분리형)", label: "원룸(분리형)", value: "원룸(분리형)" },
+    { key: "원룸(오픈형)", label: "원룸(오픈형)", value: "원룸(오픈형)" },
+    { key: "원룸(복층형)", label: "원룸(복층형)", value: "원룸(복층형)" },
+    { key: "투룸", label: "투룸", value: "투룸" },
+    { key: "투룸(복층형)", label: "투룸(복층형)", value: "투룸(복층형)" },
+    { key: "쓰리룸+", label: "쓰리룸+", value: "쓰리룸+" },
+    { key: "쓰리룸+(복층형)", label: "쓰리룸+(복층형)", value: "쓰리룸+(복층형)" },
+  ];
+  
+  export const busArray = [
+    { key: "1분", label: "1분", value: "1" },
+    { key: "3분", label: "3분", value: "3" },
+    { key: "5분", label: "5분", value: "5" },
+    { key: "7분", label: "7분", value: "7" },
+    { key: "10분", label: "10분", value: "10" },
+    { key: "15분", label: "15분", value: "15" },
+    { key: "20분", label: "20분", value: "20" },
+    { key: "30분", label: "30분", value: "30" },
+  ];
+  
+  export const subwayArray = [
+    { key: "1분", label: "1분", value: "1" },
+    { key: "3분", label: "3분", value: "3" },
+    { key: "5분", label: "5분", value: "5" },
+    { key: "7분", label: "7분", value: "7" },
+    { key: "10분", label: "10분", value: "10" },
+    { key: "15분", label: "15분", value: "15" },
+    { key: "20분", label: "20분", value: "20" },
+    { key: "30분", label: "30분", value: "30" },
+  ];
+  
+  export const workdistanceArray = [
+    { key: "10분", label: "10분", value: "10" },
+    { key: "15분", label: "15분", value: "15" },
+    { key: "20분", label: "20분", value: "20" },
+    { key: "30분", label: "30분", value: "30" },
+    { key: "45분", label: "45분", value: "45" },
+    { key: "60분", label: "60분", value: "60" },
+  ];
+        

d8516d31afd89473e139052bcdc02995b5501adc
KiChang Shin <kc7851@gmail.com>
2019-12-19 00:42:18 +0900

    로직 구현
    입력받은 array가 있을 경우 순차적으로 진행.
    리팩토링 필요 함.
    UI구성 및 화면효과 구현해야 함.

--- screens/WorldCupPlay.js
+++ screens/WorldCupPlay.js
@@ -1,19 +1,112 @@
-import React from "react";
-import { StyleSheet, View, Button, Text } from "react-native";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  View,
+  Button,
+  Text,
+  TouchableOpacity,
+  Alert
+} from "react-native";
+
+const alert = () => {
+  Alert.alert("하나를 선택해야 합니다.");
+};
 
 function WorldCupPlay({ navigation }) {
-  const goToStart = () => {
-    navigation.navigate("WorldCupStart");
-  };
-  const goToFinish = () => {
-    navigation.navigate("WorldCupFinish");
+  const [state, setState] = useState(initialState);
+
+  // const initList = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16];
+  //TODO: navigation parameter로 넘겨온 list로 변경 예정
+  const initList = [
+    { houses: 1 },
+    { houses: 2 },
+    { houses: 3 },
+    { houses: 4 },
+    { houses: 5 },
+    { houses: 6 },
+    { houses: 7 },
+    { houses: 8 }
+  ];
+
+  const goToFinish = selected => {
+    navigation.navigate("WorldCupFinish", {
+      victory: selected
+    });
   };
 
+  const initialState = {
+    survive: initList,
+    victory: [],
+    now: [initList[0], initList[1]],
+    round: 1,
+    selected: null
+  };
+
+  const selected = selectedNumber => {
+    setState({ ...state, selected: selectedNumber });
+  };
+
+  const nextRound = () => {
+    const survive = state.survive;
+    const victory = state.victory;
+    const round = state.round;
+    const selected = state.selected;
+
+    if (!selected) {
+      alert();
+      return;
+    }
+
+    if (survive.length === 2) {
+      goToFinish(selected);
+    }
+    const isNextStage = Math.floor(survive.length / 2) == round;
+
+    if (isNextStage) {
+      const newSurvive =
+        survive.length % 2 === 0
+          ? [...victory, selected]
+          : [...victory, selected, survive[survive.length - 1]];
+      setState({
+        survive: newSurvive,
+        victory: [],
+        now: [newSurvive[0], newSurvive[1]],
+        round: 1,
+        selected: null
+      });
+    } else {
+      setState({
+        ...state,
+        victory: [...victory, selected],
+        now: [survive[round * 2], survive[round * 2 + 1]],
+        round: round + 1,
+        selected: null
+      });
+    }
+  };
+
+  const stage =
+    state.survive.length === 2 ? "결승전" : state.survive.length + "강 - " + state.round + "라운드";
+    
   return (
     <View style={styles.container}>
       <Text>WorldCupPlay</Text>
-      <Button onPress={goToStart} title="시작화면" color="#841584" />
-      <Button onPress={goToFinish} title="종료화면" color="#841584" />
+      <Text>{stage}</Text>
+      <TouchableOpacity
+        onPress={() => selected(state.now[0])}
+        style={styles.selectView}
+      >
+        <Text>{state.now[0].houses}</Text>
+      </TouchableOpacity>
+      <Text>VS</Text>
+      <TouchableOpacity
+        onPress={() => selected(state.now[1])}
+        style={styles.selectView}
+      >
+        <Text>{state.now[1].houses}</Text>
+      </TouchableOpacity>
+
+      <Button onPress={nextRound} title="선택" color="#841584" />
     </View>
   );
 }
@@ -21,6 +114,13 @@ function WorldCupPlay({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  selectView: {
+    flex: 0.4,
+    width: "100%",
+    backgroundColor: "#eee",
     justifyContent: "center",
     alignItems: "center"
   }

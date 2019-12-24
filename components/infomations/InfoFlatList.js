4cbab860de77852e24866134cea61a8234c9e921
KiChang Shin <kc7851@gmail.com>
2019-12-25 01:10:39 +0900

    InfoList에서 사용할 component
    제목과 list를 만드는 component

---
+++ components/infomations/InfoFlatList.js
@@ -0,0 +1,47 @@
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  FlatList,
+  TouchableOpacity
+} from "react-native";
+import * as WebBrowser from "expo-web-browser";
+
+function InfoList({ title, array }) {
+  const open = async url => {
+    WebBrowser.openBrowserAsync(url);
+  };
+
+  return (
+    <View style={styles.componentsContainer}>
+      <Text style={{ fontSize: 24, paddingBottom: 10 }}>{title}</Text>
+      <FlatList
+        data={array}
+        renderItem={({ item }) => (
+          <View style={{ alignItems: "center" }}>
+            <TouchableOpacity
+              onPress={() => open(item.url)}
+            >
+              <Text style={styles.listItem}>{item.title}</Text>
+            </TouchableOpacity>
+          </View>
+        )}
+      />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  componentsContainer: {
+    padding: 15,
+    alignItems: "center"
+  },
+  listItem: {
+    paddingVertical: 5,
+    fontSize: 18,
+    overflow: "hidden"
+  }
+});
+
+export default InfoList;
